f459ce71bc1e31e3b9e9d434816fbab2ff4cd906
KillariDev <killari@gmail.com>
2025-11-22 13:33:25 +0200

    more uni libs, transaction explainer, stateview

--- solidity/ts/tests/hookUtils.ts
+++ solidity/ts/tests/hookUtils.ts
@@ -1,10 +1,13 @@
-import { Address } from 'viem'
+import { Address, encodeAbiParameters, keccak256 } from 'viem'
 import { ReadClient, WriteClient } from '../testsuite/simulator/utils/viem.js'
 import { getReversibleTradesHookAddress } from '../testsuite/simulator/utils/utilities.js'
-import { ReversibleTradesHook_ReversibleTradesHook } from '../types/contractArtifact.js'
+import { ReversibleTradesHook_ReversibleTradesHook, uniswap_interfaces_IPoolManager_IPoolManager } from '../types/contractArtifact.js'
+import { STATE_VIEW_ABI } from './abi.js'
 
 export const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
 export const DAI_ADDRESS = '0x6b175474e89094c44da98b954eedeac495271d0f'
+export const UNIV4_POOL_MANAGER = '0x000000000004444c5dc75cB358380D2e3dE08A90'
+export const STATE_VIEW = '0x7ffe42c4a5deea5b0fec41c94c136cf115597227'
 
 export const deployPool = async (client: WriteClient, currency0: Address, currency1: Address) => {
 	return await client.writeContract({
@@ -15,6 +18,20 @@ export const deployPool = async (client: WriteClient, currency0: Address, curren
 	})
 }
 
+type PoolKey = {
+    currency0: Address;
+    currency1: Address;
+    fee: number;
+    tickSpacing: number;
+    hooks: Address;
+}
+
+type ModifyLiquidityParams = {
+	tickLower: bigint,
+	tickUpper: bigint,
+	liquidityDelta: bigint
+}
+
 export const getPoolkey = async (client: ReadClient, currency0: Address, currency1: Address) => {
 	return await client.readContract({
 		abi: ReversibleTradesHook_ReversibleTradesHook.abi,
@@ -22,4 +39,81 @@ export const getPoolkey = async (client: ReadClient, currency0: Address, currenc
 		address: getReversibleTradesHookAddress(),
 		args: [currency0, currency1]
 	})
+}
+
+export const getPoolIdentifier = async (poolKey: PoolKey) => {
+		const encodedPoolKey = encodeAbiParameters(
+		[
+			{ name: 'currency0', type: 'address' },
+			{ name: 'currency1', type: 'address' },
+			{ name: 'fee', type: 'uint24' },
+			{ name: 'tickSpacing', type: 'int24' },
+			{ name: 'hook', type: 'address' }
+		],
+		[
+			poolKey.currency0,
+			poolKey.currency1,
+			poolKey.fee,
+			poolKey.tickSpacing,
+			poolKey.hooks
+		]
+	)
+	return keccak256(encodedPoolKey)
+}
+
+
+export const modifyPoolLiquidity = async (
+	client: WriteClient,
+	poolKey: PoolKey,
+	modifyLiquidityParams: ModifyLiquidityParams,
+	hookData: Address
+) => {
+	const transactionHash = await client.writeContract({
+		address: UNIV4_POOL_MANAGER,
+		abi: uniswap_interfaces_IPoolManager_IPoolManager.abi,
+		functionName: 'modifyLiquidity',
+		args: [
+			poolKey,
+			modifyLiquidityParams,
+			hookData
+		]
+	})
+
+	return transactionHash
+}
+export async function readV4PoolState(client: ReadClient, poolIdentifier: `0x${string}`) {
+	const liquidity = await client.readContract({
+		abi: STATE_VIEW_ABI,
+		functionName: 'getLiquidity',
+		address: STATE_VIEW,
+		args: [poolIdentifier]
+	})
+	const slot0 = await client.readContract({
+		abi: STATE_VIEW_ABI,
+		functionName: 'getSlot0',
+		address: STATE_VIEW,
+		args: [poolIdentifier]
+	})
+
+	const [sqrtPriceX96, tick, protocolFee, lpFee] = slot0
+
+	return {
+		sqrtPriceX96,
+		tick,
+		protocolFee,
+		lpFee,
+		liquidity
+	}
+}
+
+export const initiateSwap = () => {
+
+}
+
+export const executeSwap = () => {
+
+}
+
+export const reverseTrades = () => {
+
 }
--- solidity/ts/tests/test.ts
+++ solidity/ts/tests/test.ts
@@ -1,14 +1,40 @@
 import { describe, beforeEach, test } from 'node:test'
 import { getMockedEthSimulateWindowEthereum, MockWindowEthereum } from '../testsuite/simulator/MockWindowEthereum.js'
 import { createWriteClient } from '../testsuite/simulator/utils/viem.js'
-import { TEST_ADDRESSES } from '../testsuite/simulator/utils/constants.js'
-import { ensureReversibleTradesHookDeployed, setupTestAccounts } from '../testsuite/simulator/utils/utilities.js'
-import { DAI_ADDRESS, deployPool, getPoolkey, WETH_ADDRESS } from './hookUtils.js'
+import { PROXY_DEPLOYER_ADDRESS, TEST_ADDRESSES } from '../testsuite/simulator/utils/constants.js'
+import { ensureReversibleTradesHookDeployed, getReversibleTradesHookAddress, setupTestAccounts } from '../testsuite/simulator/utils/utilities.js'
+import { DAI_ADDRESS, deployPool, getPoolIdentifier, getPoolkey, readV4PoolState, STATE_VIEW, UNIV4_POOL_MANAGER, WETH_ADDRESS } from './hookUtils.js'
+import { createTransactionExplainer } from '../testsuite/simulator/utils/transactionExplainer.js'
+import { Deployment } from '../testsuite/simulator/utils/logExplaining.js'
+import { addressString } from '../testsuite/simulator/utils/bigint.js'
+import { ReversibleTradesHook_ReversibleTradesHook, uniswap_interfaces_IPoolManager_IPoolManager } from '../types/contractArtifact.js'
+import { STATE_VIEW_ABI } from './abi.js'
+
+const getDeployments = (): Deployment[] => {
+	return [{
+		abi: ReversibleTradesHook_ReversibleTradesHook.abi,
+		deploymentName: 'Reversible hook',
+		address: getReversibleTradesHookAddress(),
+	}, {
+		abi: undefined,
+		deploymentName: 'proxy depoyer',
+		address: addressString(PROXY_DEPLOYER_ADDRESS),
+	}, {
+		abi: uniswap_interfaces_IPoolManager_IPoolManager.abi,
+		deploymentName: 'UniV4 Pool manager',
+		address: UNIV4_POOL_MANAGER
+	}, {
+		abi: STATE_VIEW_ABI,
+		deploymentName: 'State View',
+		address: STATE_VIEW
+	}]
+}
 
 describe('Contract Test Suite', () => {
 	let mockWindow: MockWindowEthereum
 	beforeEach(async () => {
 		mockWindow = getMockedEthSimulateWindowEthereum()
+		mockWindow.setAfterTransactionSendCallBack(createTransactionExplainer(getDeployments()))
 		await setupTestAccounts(mockWindow)
 	})
 
@@ -16,11 +42,16 @@ describe('Contract Test Suite', () => {
 		const client = createWriteClient(mockWindow, TEST_ADDRESSES[0], 0)
 		await ensureReversibleTradesHookDeployed(client)
 	})
-	test('canDeployPoolAndGetket', async () => {
+	test('canDeployPoolAndGetKey', async () => {
 		const client = createWriteClient(mockWindow, TEST_ADDRESSES[0], 0)
 		await ensureReversibleTradesHookDeployed(client)
 		await deployPool(client, WETH_ADDRESS, DAI_ADDRESS)
 		const poolKey = await getPoolkey(client, WETH_ADDRESS, DAI_ADDRESS)
 		console.log(poolKey)
+
+		const poolIdentifier = await getPoolIdentifier(poolKey)
+		console.log(poolIdentifier)
+		const liq = await readV4PoolState(client, poolIdentifier)
+		console.log(liq)
 	})
 })
